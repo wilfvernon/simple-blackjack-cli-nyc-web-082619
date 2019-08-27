94d9c4ea7fb40436511e4981d85b07604c79034d
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-27 20:34:34 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,73 @@
+require "pry"
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(number)
+  puts "Your cards add up to #{number}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  total = 0
+  total += deal_card
+  total += deal_card
+  display_card_total(total)
+  total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
+
+def hit?(total)
+  prompt_user
+  input = get_user_input.strip
+  #binding.pry
+  if input == 'h'
+    total += deal_card
+  elsif input == 's'
+    total
+     #binding.pry
+  else
+    invalid_command
+    prompt_user
+    input = get_user_input
+  # binding.pry 
+  end
+end
+
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  total = initial_round
+  #binding.pry
+  until total > 21
+    total = hit?(total)
+    #binding.pry
+    display_card_total(total)
+    #binding.pry
+  end
+  end_game(total)
 end
     
